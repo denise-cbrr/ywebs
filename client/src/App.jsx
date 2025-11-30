18e5fd732a4b429f7401c0d4d129c5e6aac86f5f
Denise Cabrera <cabrera.denisep@gmail.com>
2025-11-29 22:59:17 -0500

    reverted and installed sql

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          git push --force origin main        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
